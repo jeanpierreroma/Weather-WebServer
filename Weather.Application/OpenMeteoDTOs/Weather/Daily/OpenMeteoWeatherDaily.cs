b8efb8db7ed85d65e28cfcf1216622bdd1d4e004
Roman <ppgora352@gmail.com>
2025-10-23 12:19:00 +0300

    refactor: update weather DTOs and model definitions

--- Weather.Application/OpenMeteoDTOs/Weather/Daily/OpenMeteoWeatherDaily.cs
+++ Weather.Application/OpenMeteoDTOs/Weather/Daily/OpenMeteoWeatherDaily.cs
@@ -6,25 +6,40 @@ public sealed class OpenMeteoWeatherDaily
 {
     [JsonPropertyName("time")]
     public List<string> Time { get; init; } = new();
-
-    [JsonPropertyName("uv_index_max")]
-    public List<double?> UvIndexMax { get; init; } = new();
-
+    
+    [JsonPropertyName("temperature_2m_mean")]
+    public List<double?> TemperatureMean { get; init; } = new();
+    
+    [JsonPropertyName("apparent_temperature_mean")]
+    public List<double?> ApparentTemperatureMean { get; init; } = new();
+    
     [JsonPropertyName("sunrise")]
     public List<string> Sunrise { get; init; } = new();
 
     [JsonPropertyName("sunset")]
     public List<string> Sunset { get; init; } = new();
 
+    [JsonPropertyName("uv_index_max")]
+    public List<double?> UvIndexMax { get; init; } = new();
+
     [JsonPropertyName("precipitation_sum")]
     public List<double?> PrecipitationSum { get; init; } = new();
+    
+    [JsonPropertyName("visibility_mean")]
+    public List<double?> VisibilityMean { get; init; } = new();
 
-    [JsonPropertyName("wind_speed_10m_max")]
-    public List<double?> WindSpeed10MMax { get; init; } = new();
-
-    [JsonPropertyName("wind_gusts_10m_max")]
-    public List<double?> WindGusts10MMax { get; init; } = new();
-
-    [JsonPropertyName("wind_direction_10m_dominant")]
-    public List<int?> WindDirection10MDominant { get; init; } = new();
+    [JsonPropertyName("winddirection_10m_dominant")]
+    public List<int?> WindDirectionDominant { get; init; } = new();
+    
+    [JsonPropertyName("wind_gusts_10m_mean")]
+    public List<double?> WindGustsMean { get; init; } = new();
+    
+    [JsonPropertyName("wind_speed_10m_mean")]
+    public List<double?> WindSpeedMean { get; init; } = new();
+    
+    [JsonPropertyName("relative_humidity_2m_mean")]
+    public List<int?> RelativeHumidityMean { get; init; } = new();
+    
+    [JsonPropertyName("surface_pressure_mean")]
+    public List<double?> SurfacePressureMean { get; init; } = new();
 }
--- Weather.Application/OpenMeteoDTOs/Weather/Daily/OpenMeteoWeatherDailyUnits.cs
+++ Weather.Application/OpenMeteoDTOs/Weather/Daily/OpenMeteoWeatherDailyUnits.cs
@@ -6,25 +6,40 @@ public sealed class OpenMeteoWeatherDailyUnits
 {
     [JsonPropertyName("time")]
     public string Time { get; init; } = string.Empty;
-
-    [JsonPropertyName("uv_index_max")]
-    public string UvIndexMax { get; init; } = string.Empty;
-
+    
+    [JsonPropertyName("temperature_2m_mean")]
+    public string TemperatureMean { get; init; } = string.Empty;
+    
+    [JsonPropertyName("apparent_temperature_mean")]
+    public string ApparentTemperatureMean { get; init; } = string.Empty;
+    
     [JsonPropertyName("sunrise")]
     public string Sunrise { get; init; } = string.Empty;
 
     [JsonPropertyName("sunset")]
     public string Sunset { get; init; } = string.Empty;
 
+    [JsonPropertyName("uv_index_max")]
+    public string UvIndex { get; init; } = string.Empty;
+
     [JsonPropertyName("precipitation_sum")]
     public string PrecipitationSum { get; init; } = string.Empty;
-
-    [JsonPropertyName("wind_speed_10m_max")]
-    public string WindSpeed10MMax { get; init; } = string.Empty;
-
-    [JsonPropertyName("wind_gusts_10m_max")]
-    public string WindGusts10MMax { get; init; } = string.Empty;
-
-    [JsonPropertyName("wind_direction_10m_dominant")]
-    public string WindDirection10MDominant { get; init; } = string.Empty;
+    
+    [JsonPropertyName("visibility_mean")]
+    public string VisibilityMean { get; init; } = string.Empty;
+    
+    [JsonPropertyName("winddirection_10m_dominant")]
+    public string WindDirectionDominant { get; init; } = string.Empty;
+    
+    [JsonPropertyName("wind_gusts_10m_mean")]
+    public string WindGustsMean { get; init; } = string.Empty;
+    
+    [JsonPropertyName("wind_speed_10m_mean")]
+    public string WindSpeedMean { get; init; } = string.Empty;
+    
+    [JsonPropertyName("relative_humidity_2m_mean")]
+    public string RelativeHumidityMean { get; init; } = string.Empty;
+    
+    [JsonPropertyName("surface_pressure_mean")]
+    public string SurfacePressureMean { get; init; } = string.Empty;
 }
